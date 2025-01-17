eb192d231dff273fa2256880807337b1204e058a
xvw <xavier.vdw@gmail.com>
2025-01-17 15:41:48 +0100

    Also accept `jpg` as an avatar

--- lib/action/images.ml
+++ lib/action/images.ml
@@ -2,7 +2,7 @@ let run (module R : Sigs.RESOLVER) cache =
   (* We pass [cache] in order to add more rules. *)
   let open Yocaml.Eff in
   cache
-  |> Batch_copy.run ~extension:[ "svg"; "png" ] ~source:R.Source.static_images
-       ~target:R.Target.images
-  >>= Batch_copy.run ~extension:[ "svg"; "png" ] ~source:R.Source.avatars
+  |> Batch_copy.run ~extension:[ "svg"; "png"; "jpg" ]
+       ~source:R.Source.static_images ~target:R.Target.images
+  >>= Batch_copy.run ~extension:[ "svg"; "png"; "jpg" ] ~source:R.Source.avatars
         ~target:R.Target.avatars
